3b64fe563aa124ed29bcce2499b3f4107e0ca9d1
bakasibruceto <abla09027@gmail.com>
2024-01-22 05:44:07 +0800

    createExpenseRow

--- js/frontend.js
+++ js/frontend.js
@@ -1,3 +1,6 @@
+import { createCell, createButtonCell } from "./methods.js";
+
+//table row for each category
 export function createCategoryElements(category) {
     let categoryRow = document.createElement('tr');
     let categoryCell = document.createElement('td');
@@ -7,4 +10,13 @@ export function createCategoryElements(category) {
     categoryRow.appendChild(categoryCell);
 
     return { categoryRow, categoryCell };
+}
+
+//table row for each expense
+export function createExpenseRow(expense) {
+    let row = document.createElement('tr');
+    createCell(row, expense.name, 'text-left '); 
+    createCell(row, expense.amount, 'text-right pl-5 pr-5');
+    // createButtonCell(row, "edit", "additional-classes");
+    return row;
 }
