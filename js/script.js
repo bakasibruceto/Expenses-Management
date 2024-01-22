57d4ccf0e17a15c3d6f55e680398392f1f256df4
bakasibruceto <abla09027@gmail.com>
2024-01-23 01:13:04 +0800

    index script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 import './daily.js';
-// import { totalExpenses } from './daily.js';
+import { createTotalExpenseRow } from './frontend.js';
 
-// fetchDaily().then(totalExpenses => {
-//     document.getElementById('exp').innerHTML = totalExpenses;
-//     console.log(totalExpenses);
-// });
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        let totalExpensesPerId = data.entries.map(entry => ({
+            id: entry.id,
+            date: new Date(`${entry.year}-${entry.month}-${entry.date}`),
+            day: entry.day,
+            event: entry.event,
+            totalExpenses: entry.TotalDayExpenses,
+            expenses: entry.expenses // Assuming each entry has an expenses array
+        }));
+
+        let tableBody = document.getElementById('tableSummaryBody');
+
+        // Loop through each entry and add a row to the table for each expense
+        totalExpensesPerId.forEach(TotalExpesesPerDay => {
+            let row = createTotalExpenseRow(TotalExpesesPerDay);
+            tableBody.appendChild(row);
+        });
+
+        console.log(totalExpensesPerId);
+    })
+    .catch(error => console.error('Error:', error));
