445ac55f19e38412bd1ef91bcec1f19d87279430
bakasibruceto <abla09027@gmail.com>
2024-01-23 01:12:54 +0800

    added formats

--- js/methods.js
+++ js/methods.js
@@ -5,6 +5,12 @@ export function getCategory(data, category) {
         .map(expense => ({ name: expense.name, amount: expense.amount }));
 }
 
+export function getId(data, entryId) {
+    return data.entries
+        .flatMap(entry => entry.id)
+        .map(expense => ({ id: expense.id }));
+}
+
 export function createCell(row, textContent, className) {
     let cell = document.createElement('td');
     cell.textContent = textContent;
@@ -21,4 +27,19 @@ export function createButtonCell(row, textContent, className) {
     button.className = 'px-4  bg-blue-500 text-white rounded ' + className;
     cell.appendChild(button);
     row.appendChild(cell);
+}
+
+export function formattedDate(date) {
+    return date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+    })
+}
+
+export function formattedCurrency(amount) {
+    return amount.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'PHP'
+    })
 }
