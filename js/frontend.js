b8bf398ff89e5e6cd0593a8c44cfe92cf599daa9
bakasibruceto <abla09027@gmail.com>
2024-01-23 01:12:41 +0800

    Update frontend.js
    expenses summary

--- js/frontend.js
+++ js/frontend.js
@@ -1,4 +1,4 @@
-import { createCell, createButtonCell } from "./methods.js";
+import { createCell, formattedCurrency, formattedDate } from "./methods.js";
 
 //table row for each category
 export function createCategoryElements(category) {
@@ -15,8 +15,20 @@ export function createCategoryElements(category) {
 //table row for each expense
 export function createExpenseRow(expense) {
     let row = document.createElement('tr');
-    createCell(row, expense.name, 'text-left '); 
+    createCell(row, expense.name, 'text-left ');
     createCell(row, expense.amount, 'text-right pl-5 pr-5');
     // createButtonCell(row, "edit", "additional-classes");
     return row;
 }
+
+//index table
+export function createTotalExpenseRow(entry) {
+    let row = document.createElement('tr');
+    row.id = `${entry.id}`;
+    createCell(row, formattedDate(entry.date), 'text-left ');
+    createCell(row, entry.day, 'text-left ');
+    createCell(row, entry.event, 'text-left ');
+    createCell(row, formattedCurrency(entry.totalExpenses), 'text-left ');
+    return row;
+}
+
